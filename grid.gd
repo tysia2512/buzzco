b69caf35887608bc48ee87a2676f919d555727b1
MS <ms@MSs-MacBook-Pro.beefbox>
2025-06-22 15:01:59 +0200

    Refactor grid drawing

--- grid.gd
+++ grid.gd
@@ -1,40 +1,36 @@
 extends Node2D
 
-var GridTile = preload("res://scenes/grid_tile.tscn")
-#@export var gridTile: GridTile
+@onready var GridTile = preload("res://scenes/grid_tile.tscn")
 
-
-#var ROWS = 6
-#var COLUMNS = 2
 const FLIPPED = false
 const ROWS = [4, 3, 4, 3, 4]
+@onready var grid = []
 
 func _ready():
-	var sample_tile = GridTile.instantiate()
-	var h = sample_tile.get_texture_size().y
-	var w = sample_tile.get_texture_size().x
-	print(h)
-	print(w)
+	grid.resize(ROWS.size())
 	for i in range(0, ROWS.size()):
-		var y = position.y + h * i / 2
-		if i % 2 == 1:
-			continue
+		grid[i] = []
+		grid[i].resize(ROWS[i])
 		
-		for j in range(0, ROWS[i]):
-			var grid_tile = GridTile.instantiate()
-			grid_tile.position.x = position.x + j * w * 3 / 2
-			grid_tile.position.y = y
-			add_child(grid_tile)
-			
 	for i in range(0, ROWS.size()):
-		var y = position.y + h * i / 2
-		if i % 2 == 0:
-			continue
-		
 		for j in range(0, ROWS[i]):
-			var grid_tile = GridTile.instantiate()
-			grid_tile.position.x = position.x + j * w * 3 / 2 + w * 3 / 4
-			grid_tile.position.y = y
-			add_child(grid_tile)
+			_add_grid_tile(i, j)
 		
+func _add_grid_tile(row: int, column: int):
+	var tile = GridTile.instantiate()
+	tile.visible = false
+	add_child(tile)
+	var h = tile.get_texture_size().y
+	var w = tile.get_texture_size().x
+	print("height: ", h, " width: ", w)
 	
+	var y = position.y + h * row / 2
+	var x = position.x + column * w * 3 / 2
+	if row % 2 == 1:
+		x += w * 3 / 4
+		
+	tile.position.x = x
+	tile.position.y = y
+	grid[row][column] = tile
+	tile.set_grid_position(row, column)
+	tile.visible = true
--- scripts/grid_tile.gd
+++ scripts/grid_tile.gd
@@ -1,4 +1,13 @@
-extends Node2D
+class_name GridTile extends Node2D
+
+@export var row: int = 0
+@export var column: int = 0
+
+@onready var sprite: Sprite2D = $Sprite2D
 
 func get_texture_size():
-	return get_child(0).get_texture_size()
+	return sprite.get_texture_size() 
+	
+func set_grid_position(_row: int, _column: int):
+	row = _row
+	column = _column
