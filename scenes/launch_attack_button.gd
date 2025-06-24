26712fba7182a551b1598dc2d35e1e92d131b6b0
MS <ms@MSs-MacBook-Pro.beefbox>
2025-06-24 22:16:14 +0200

    Add pollen reset after the assault is launched

---
+++ scenes/launch_attack_button.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _button_pressed():
+	PollenManager.launch_assault()
--- scenes/pollen_manager.gd
+++ scenes/pollen_manager.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-var pollen = 50
+var pollen = 5
+var _pollen_regained_from_launch = 3
 
 func can_afford_pollen(x: int) -> bool:
 	return true
 
 func pay_pollen(x: int) -> void:
 	pollen -= x
+
+func launch_assault():
+	pollen += _pollen_regained_from_launch
