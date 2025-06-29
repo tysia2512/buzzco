f46c5e0d76f25ef6163b6684ca70b8c448150464
MS <ms@MSs-MacBook-Pro.local>
2025-06-29 20:20:01 +0200

    Disable launch assault button if it's enemies' turn

--- scripts/launch_attack_button.gd
+++ scripts/launch_attack_button.gd
@@ -7,3 +7,5 @@ func _pressed() -> void:
 	PollenManager.add_pollen(GameState.POLLEN_RECOVERED_ON_ASSAULT)
 	perform_player_action.emit()
 	
+func _process(delta: float):
+	disabled =! GameState.is_player_turn()
