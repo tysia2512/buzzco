ae7535ef69fb83c82277621df59732b72d1dec6e
Martyna Siejba <tysia2512>
2025-11-08 21:29:30 +0100

    Show the store every other game

--- scripts/game_loop.gd
+++ scripts/game_loop.gd
@@ -23,11 +23,11 @@ func _on_start_game_button_pressed() -> void:
 func _on_level_play_level_cleared() -> void:
 	level += 1
 	level_play.queue_free()
-	# if level % 2 == 1 or:
-	card_shop.load()
-	card_shop.visible = true
-	# else:
-	# 	_show_next_level_page()
+	if level % 2 == 1:
+		card_shop.load()
+		card_shop.visible = true
+	else:
+		_show_next_level_page()
 
 func _on_card_shop_card_shop_closed() -> void:
 	card_shop.visible = false
